5c2aac03b338976a92a7d5795b2e45645dca9cbd
Sashi Bhumaraju <sasi.bhumaraju@gmail.com>
2023-08-08 17:54:06 +0530

    pwa converted from web

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+              src:"/logo192.png",
+              sizes:"192x192",
+              type:"image/png"
+          },
+  
+          {
+              src:"/logo256.png",
+              sizes:"256x256",
+              type:"image/png"
+          },
+          {
+              src:"/logo384.png",
+              sizes:"384x384",
+              type:"image/png"
+          },
+          {
+              src:"/logo512.png",
+              sizes:"512x512",
+              type:"image/png",
+              purpose:"any maskable"
+          }]
+      }
+    })
+  ],
   build: {
     outDir: 'build', // Replace 'custom-dist' with your desired folder name
   },
